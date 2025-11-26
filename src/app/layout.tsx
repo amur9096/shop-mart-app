aadf08969b1dd606c26d049167204579e7aac549
Amr Khalid <amor.khalid9096@gmail.com>
2025-11-27 01:07:58 +0200

    start of work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Button } from "@/components/ui/button";
+import Navbar from "@/components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navbar />
+        <div className="container mx-auto">{children}</div>
         <Button> click me </Button>
       </body>
     </html>
