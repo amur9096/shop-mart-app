0f2275625c43142c72997477c93468803ad1e29f
Amr Khalid <amor.khalid9096@gmail.com>
2025-11-27 00:55:10 +0200

    adding shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Button } from "@/components/ui/button";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Button> click me </Button>
       </body>
     </html>
   );
