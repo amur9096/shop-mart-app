2296d938d03735ba66d8b50d5df6d92922e4bae9
Amr Khalid <amor.khalid9096@gmail.com>
2025-12-10 20:29:33 +0200

    signIn Page

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,9 +1,117 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Card } from "@/components/ui/card";
+import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .nonempty("email is required")
+    .min(3, "email must be at least 3 characters")
+    .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "invalid email"),
+
+  password: z
+    .string()
+    .nonempty("password is required")
+    .min(8, "password must be at least 8 characters")
+    .max(20, "password must be at most 20 characters")
+    .regex(
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+      "password must contain at least one uppercase letter, one lowercase letter, one number and one special character"
+    ),
+});
+
+type FormValues = z.infer<typeof formSchema>;
 
 export default function Login() {
+  let searchParams = useSearchParams()
+  console.log(searchParams.get('error'));
+
+
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: FormValues) {
+    const res = await signIn('credentials', {
+      email: values.email,
+      password: values.password,
+      redirect: true,
+      callbackUrl: "/",
+    });
+    console.log(values);
+  }
+
   return (
-    <div>
-      Login Page
-    </div>
-  )
+    <>
+      <div className="flex flex-col justify-center items-center min-h-[75vh]">
+        <h1 className="text-4xl font-bold mb-8">Login Page</h1>
+
+        <Card className="p-5 w-full max-w-md">
+          <Form {...form}>
+            {searchParams.get('error') && <p className="text-red-500">{searchParams.get('error')}</p>}
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Amr@example.com" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Password"
+                        {...field}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button className="w-full cursor-pointer " type="submit">
+                Submit
+              </Button>
+            </form>
+          </Form>
+        </Card>
+      </div>
+    </>
+  );
 }
