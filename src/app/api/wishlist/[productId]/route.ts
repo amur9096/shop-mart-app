33eba408aae9822df115a900c4c7ad41d5e48e53
Amr Khalid <amor.khalid9096@gmail.com>
2025-12-25 17:38:11 +0200

    first commit

--- src/app/api/wishlist/[productId]/route.ts
+++ src/app/api/wishlist/[productId]/route.ts
@@ -1,14 +1,15 @@
 import { getUserToken } from "@/app/Helpers/getUserToken";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(
-  _req: Request,
-  { params }: { params: { productId: string } }
+  _req: NextRequest,
+  { params }: { params: Promise<{ productId: string }> }
 ) {
+  const { productId } = await params;
   const token = await getUserToken();
 
   const res = await fetch(
-    `https://ecommerce.routemisr.com/api/v1/wishlist/${params.productId}`,
+    `https://ecommerce.routemisr.com/api/v1/wishlist/${productId}`,
     {
       method: "DELETE",
       headers: { token },
